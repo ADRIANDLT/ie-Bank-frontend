8869c07d544e5a8462700383ee2d2f3f785c5ac2
ADRIANDLT <adrian.delatorre@outlook.com>
2024-09-30 22:31:30 +0200

    Baseline as provided

---
+++ src/components/Skull.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Skull</h1>
+        <p>{{ msg }}</p>
+        <button type="button" class="btn btn-secondary">
+            Skull button
+        </button>
+
+    </div>
+</template>
+ 
+<script>
+import axios from 'axios'
+export default
+    {
+        name: 'Skull',
+        data() {
+            return {
+                msg: `Hi! This is the skull component 💀, running in mode: ${process.env.NODE_ENV} with environment variables defined in ${process.env.VUE_APP_ENV_VAR_FILE_NAME}`
+            }
+        },
+        methods:
+        {
+            getSkull() {
+                const path = `${process.env.VUE_APP_ROOT_URL}/skull`;
+                axios.get(path)
+                    .then((response) => {
+                        this.msg = response.data;
+                        this.environment = process.env.NODE_ENV;
+                        this.env_var_file_name = process.env.VUE_APP_ENV_VAR_FILE_NAME
+                    })
+                    .catch(error => console.log(error))
+            }
+        },
+        created() {
+            this.getSkull()
+        }
+
+
+    }
+</script>
